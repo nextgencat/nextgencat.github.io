54c4a3582a9429c3ac276fbbc4960e12d34d37cc
nextgencat <cavabanga027@gmail.com>
2026-01-30 19:09:05 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const tg_link = "https://t.me/nextgencat";
 const dc_link = "https://discord.gg/"
 const gh_link = "https://github.com/nextgencat"
 
-const text = "very interesting here.. My name is sigma, but you can call me Hustavo";
+const text = "haii!! my name is nextgencat. Now im learning html, css & js";
 const el = document.getElementById("desc");
 let i = 0;
 
@@ -26,4 +26,5 @@ function dc() {
 
 function gh() {
     window.open(gh_link, '_blank')
+
 }
